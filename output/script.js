2a0992ddbe403e8db5bb265b596cd20888656d17
Soyeb <tssoyeb@gmail.com>
2023-10-27 09:21:45 +0600

    basic

--- output/script.js
+++ output/script.js
@@ -1,3 +1,7 @@
 "use strict";
-const country = 'I love Bangladesh';
-console.log(country);
+let studentName = 'soyeb';
+// studentName=34; //here Im getting error because in Ts when you declare and assign at the same line it will take the assigned data type permanently
+console.log(studentName);
+let number;
+number = 34;
+number = 'aabir'; //there wont be any error because we didnt assign at the same line where the variable declared
