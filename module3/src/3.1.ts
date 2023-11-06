f5019e934620017baef2e2de880d1a1026567662
Soyeb <tssoyeb@gmail.com>
2023-11-06 22:06:53 +0600

    class

---
+++ module3/src/3.1.ts
@@ -0,0 +1,25 @@
+{
+    // class
+    class Animal {
+        // name: string;
+        // species: string;
+        // sound: string;
+
+        //by using parameter properties we dont need to declare properties and assigning value in properties with this key word
+        constructor(public name: string, public species: string, public sound: string) {
+            // this.name = name;
+            // this.species = species;
+            // this.sound = sound;
+        }
+        makeSound() {
+            console.log(`The name is ${this.name} and sound ${this.sound}`);
+        }
+
+    }
+    const dog = new Animal('Tomy', 'dog', 'gew')
+    dog.makeSound()
+    console.log(dog);
+
+
+    //
+}
